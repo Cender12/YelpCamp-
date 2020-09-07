f04d1252ab5e20189dda39c4d4fcd4fb913c80e0
chris e <enderchristopher@gmail.com>
2020-09-07 23:36:59 +0000

    add procfile and listen port to app.js

--- v1/app.js
+++ v1/app.js
@@ -66,6 +66,7 @@ app.use('/campgrounds/:id/comments', commentRoutes);
 
 
 //PORT listen
-app.listen(3000, function(){
-	console.log('The YelpCamp Server has Started');
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
 });
